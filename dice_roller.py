32e0cf93ccab6b073aee1fd316556ebbf9b715c4
Marwa7246 <marwa.ragheb@gmail.com>
2020-11-14 12:12:52 -0500

    Save 2 players' rolls sum

--- dice_roller.py
+++ dice_roller.py
@@ -1,19 +1,48 @@
 import random
 def main():
 
-  dice_rolls =int(input('How many dice you want to roll? '))
+
+
+  dice_rolls =2*int(input('How many dice each player want to roll? '))
   dice_size = int(input('How many sides are the dice? '))
-  dice_sum = 0
+
+  team1 = input('Enter First Team name: ')
+  team2 = input('Enter second Team name: ')
+
+  team1_dice_sum = 0
+  team2_dice_sum = 0
+
+  team1_rolls=[]
+  team2_rolls=[]
+
   for i in range(0, dice_rolls):
+
     roll = random.randint(1, dice_size)
-    if roll == 1:
-      print(f'You rolled a {roll}! Critical Fail')
-    elif roll == dice_size:
-      print(f'You rolled a {roll}! Critical Success!')
+    if (i % 2) == 0:
+      team1_dice_sum += roll
+      team1_rolls.append(team1_dice_sum)
+      if roll == 1:
+        print(f'{team1} rolled a {roll}! Critical Fail')
+      elif roll == dice_size:
+        print(f'{team1} rolled a {roll}! Critical Success!')
+      else:
+        print(f'{team1} rolled a {roll}')    
+      print(f'{team1} rolled a sum of {team1_dice_sum}')
+
     else:
-      print(f'You rolled a {roll}')    
-    dice_sum += roll
-    print(f'You rolled a sum of {dice_sum}')
+      team2_dice_sum += roll
+      team2_rolls.append(team2_dice_sum)
+      if roll == 1:
+        print(f'{team2} rolled a {roll}! Critical Fail')
+      elif roll == dice_size:
+        print(f'{team2} rolled a {roll}! Critical Success!')
+      else:
+        print(f'{team2} rolled a {roll}')    
+      print(f'{team2} rolled a sum of {team2_dice_sum}')      
+
+
+  print(f'{team1} rolls sum are: {team1_rolls}')
+  print(f'{team2} rolls sum are: {team2_rolls}')
 
 if __name__== "__main__":
   main()
